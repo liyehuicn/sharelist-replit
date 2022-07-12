40c5768d67448c2193ee398cd60c8872de52f0a4
liyehui <liyehui@qq.com>
2022-07-12 16:07:42 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #下载二进制程序
 # rm -rf alist* data/ #Uncomment this line to update
 if [ ! -f "sharelist" ];then
-    curl -L https://gh.ddlc.top/https://github.com/reruin/sharelist/releases/download/v0.3.15/sharelist_linux_amd64.tar.gz -o alist.tar.gz
+    curl -L https://gh.ddlc.top/https://github.com/liyehuicn/sharelist-replit/blob/main/sharelist_linux_amd64.tar.gz -o alist.tar.gz
   tar -zxvf alist.tar.gz
   rm -f alist.tar.gz
   #mv alist-linux-musl-amd64 alist
